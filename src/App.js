0d6cfed2d6b2d78d4f70405383123492ad974e17
gawhar <gawharsalman@outlook.com>
2024-10-29 13:02:15 +0530

    deleted:    public/favicon.ico 	new file:   public/images/A dish from Warehouse London.jpg 	new file:   public/images/Blue & Green Matcha Lattes.jpg 	new file:   public/images/Lasagna Food.jpg 	new file:   public/images/Methi (Fenugreek) Vada - Indian Cuisine.jpg 	new file:   public/images/Pav bhaji.jpg 	new file:   public/images/Poori Indian snack.jpg 	new file:   public/images/Strawberry cocktail drinks.jpg 	new file:   public/images/burger-with-vegetable.jpg 	new file:   public/images/fried chicken.jpg 	new file:   public/images/juicy lemon drink.jpg 	new file:   public/images/ramen-seafood-noodles.jpg 	new file:   public/images/strawbey cocktail.jpg 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/food.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { food } from "./food";
 
-function App() {
+const App = () => {
+  const [menu, setmenu] = useState(food);
+
+  const menuHandler = (category) => {
+    const newMenu = food.filter((item) => category === item.category);
+    setmenu(newMenu);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Food Menu</h1>
+        <div>
+          <button className="btn" onClick={() => setmenu(food)}>
+            All
+          </button>
+          <button className="btn" onClick={() => menuHandler("lunch")}>
+            Lunch
+          </button>
+          <button className="btn" onClick={() => menuHandler("dinner")}>
+            Dinner
+          </button>
+          <button className="btn" onClick={() => menuHandler("drinks")}>
+            Drinks
+          </button>
+        </div>
       </header>
-    </div>
+      <section>
+        {menu.map((item) => {
+          return <Item key={item.id} item={item}></Item>;
+        })}
+      </section>
+    </>
   );
-}
+};
+
+const Item = (props) => {
+  const { img, name, price } = props.item;
+  const [show, setShow] = useState(false);
+  const titleCase = (name) => {
+    return name
+      .toLowerCase()
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  };
+  return (
+    <article>
+      <img src={img} alt={name}></img>
+      <h2>{titleCase(name)}</h2>
+      <div>
+        <p className="price">{price}$</p>
+        <button className="show_hide_btn" onClick={() => setShow(!show)}>
+          {show ? "Know Less" : "Know More"}
+        </button>
+      </div>
+      <div>
+        {show && (
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sapien
+            lacus, sagittis ut lacinia nec, hendrerit eu ex. Fusce velit magna,
+            convallis in ultricies eu, scelerisque quis mauris. Vivamus augue
+            sem, consectetur sit amet aliquet at, ultrices eu metus. Mauris a
+            malesuada ligula, dapibus facilisis erat.
+          </p>
+        )}
+      </div>
+    </article>
+  );
+};
 
 export default App;
